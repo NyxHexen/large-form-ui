538c2dc7d11c8bcd7c2ec8bfa550173279dbd2ec
NyxHexen <jd.davis948@gmail.com>
2023-09-17 19:42:15 +0100

    FIX: added missing field key;

--- generateTestableFields.js
+++ generateTestableFields.js
@@ -1,7 +1,7 @@
 function generateTestableFields(n) {
   let fields = [];
   for (let i = 0; i < n + 1; i++) {
-    fields[i] = { name: `Field ${i + 1}`, id: `field_${i + 1}` };
+    fields[i] = { name: `Field ${i + 1}`, id: `field_${i + 1}`, type: "text" };
   }
 
   return fields;
