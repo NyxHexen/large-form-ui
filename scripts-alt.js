3a7b571eeb481fac384730cbe5a3ecdf6f864b8a
NyxHexen <jd.davis948@gmail.com>
2023-09-17 12:14:54 +0100

    CHANGE: decided to move away from having two columns, to using one columns with click effects;

--- scripts-alt.js
+++ scripts-alt.js
@@ -3,119 +3,87 @@ import generateRandomFields from "./generateRandomFields.js";
 class ManyFieldsFormManager {
   constructor() {
     this.fields = generateRandomFields(32);
-    this.DEFAULT_LENGTH = 11;
+    this.PIP_LENGTH = 21;
+    this.FORM_LENGTH = 11;
 
-    this.availFieldsDiv = document.querySelector(".avail-fields");
-    this.availFieldsList = document.querySelector(".avail-fields-list");
-    this.activeFieldsList = document.querySelector(".active-fields-list");
+    this.pipsDiv = document.querySelector(".pips");
+    this.pipsList = document.querySelector(".pips-list");
     this.searchForm = document.querySelector(".flds-container>div");
-    this.currentList;
   }
 
   init() {
     if (this.fields) {
-      const pipEl = this.fields.map((field) =>
-        this.pipMng({ field, list: this.availFieldsList, action: "add" })
-      );
-      this.currentList = undefined;
+      const tempPipsList = this.fields.map((field) => {
+        this.pipMng(field, "add");
+      });
     }
   }
 
-  pipGen(name, icon) {
+  pipGen(field) {
     const li = document.createElement("li");
+    li.classList = `${field.id}-pip ps-1 pe-1 mb-2`;
     li.innerHTML = `
-    <i class="fa-solid fa-${
-      icon === "plus" ? "plus text-success" : "minus text-danger"
-    } ps-1"></i>
-    <span class="ps-3 pe-2">${name}</span>
+    <i class="fa-solid fa-plus text-success ps-1"></i>
+    <span class="ps-3 pe-2">${field.name}</span>
     `;
     return li;
   }
 
-  pipMng(args) {
-    const { field, list, action } = args;
-    const isActiveList = list.classList.contains(`active-fields-list`);
-    const newPip = this.pipGen(field.name, isActiveList ? "minus" : "plus");
-    const fieldEl = document.querySelector(`[id*=${field.id}]`);
-    newPip.classList = `${
-      (isActiveList ? "active" : "avail") + "_" + field.id
-    } field-${action} ps-1 mb-2`;
+  pipMng(field, action) {
+    if (field && action == "add") {
+      const newPip = this.pipGen(field);
+      const fieldEl = document.querySelector(`[id*=${field.id}]`);
 
-    // To-Do - Add position to each field so it can be organized additionally,
-    // and will help with positioning/sorting.
-    // To-Do - Reset button.
-    // To-Do  Create buttons to organize the pips, "A-Z" or "Z-A".
+      const updateField = () => {
+        // To-Do - Find a way to do this without having to reset pipsList
+        // each time
+        // To-Do - Find a way to have pips that are being added back by removing a field
+        // to be added all the way back. If the first ul is empty - get rid of it and add
+        // a new one at the back.
+        this.fieldMng({
+          field,
+          action: fieldEl ? "remove" : "add",
+        });
+      };
 
-    const setField = () => {
-      // To-Do - Find a way to do this without having to reset currentList
-      // each time
-      // To-Do - Find a way to have pips that are being added back by removing a field
-      // to be added all the way back. If the first ul is empty - get rid of it and add
-      // a new one at the back.
-      this.fieldMng({
-        field,
-        action: fieldEl ? "remove" : "add",
+      newPip.addEventListener("click", () => {
+        updateField();
       });
-      this.pipMng({
-        field,
-        list: isActiveList ? this.activeFieldsList : this.availFieldsList,
-        action: "remove",
-      });
-      this.currentList = undefined;
-      this.pipMng({
-        field,
-        list: isActiveList ? this.availFieldsList : this.activeFieldsList,
-        action: "add",
-      });
-      this.currentList = undefined;
-    };
 
-    newPip.addEventListener("click", () => {
-      setField();
-    });
+      let pipLists = this.pipsList.parentElement.querySelectorAll("ul");
+      let tempList = [];
 
-    if (!this.currentList) {
-      this.currentList = list;
-    }
-
-    let pipLists = this.currentList.parentElement.querySelectorAll("ul");
-    let tempList = [];
-
-    if (action == "add") {
-      if ((this.currentList.children.length + 1) % this.DEFAULT_LENGTH != 0) {
-        this.currentList.append(newPip);
+      if ((this.pipsList.children.length + 1) % this.PIP_LENGTH != 0) {
+        this.pipsList.append(newPip);
       } else {
         const newCol = document.createElement("ul");
-        newCol.classList = "avail-fields-list p-0 me-2";
+        newCol.classList = "pips-list p-0 me-2";
 
         if (pipLists.length > 1) {
           let minIndex;
 
           for (let i = 0; i < pipLists.length; i++) {
-            if (pipLists[i].children.length < this.DEFAULT_LENGTH - 1) {
+            if (pipLists[i].children.length < this.PIP_LENGTH - 1) {
               minIndex = i;
             }
           }
 
           if (minIndex == undefined) {
-            this.currentList.after(newCol);
-            this.currentList = newCol;
-            this.currentList.append(newPip);
+            this.pipsList.after(newCol);
+            this.pipsList = newCol;
+            this.pipsList.append(newPip);
           } else {
-            this.currentList = pipLists[minIndex];
-            this.currentList.append(newPip);
+            this.pipsList = pipLists[minIndex];
+            this.pipsList.append(newPip);
           }
         } else {
-          this.currentList.after(newCol);
-          this.currentList = newCol;
-          this.currentList.append(newPip);
+          this.pipsList.after(newCol);
+          this.pipsList = newCol;
+          this.pipsList.append(newPip);
         }
       }
-    } else if (action == "remove") {
-      let pipToDel = document.querySelector(
-        `[class*=${(isActiveList ? "active_" : "avail_") + field.id}]`
-      );
-      pipToDel.remove();
+    } else if (field && action == "update") {
+      console.log("Update!");
     }
   }
 
@@ -154,9 +122,7 @@ class ManyFieldsFormManager {
     `;
   }
 
-  fieldMng(args) {
-    let { action, field } = args;
-
+  fieldMng(action, field) {
     // Check if both action and field are provided
     if (action && field) {
       if (action == "add") {
